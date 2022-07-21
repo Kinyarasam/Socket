beab875b4588242ce0771e9010a2c1e2b9969af9
kinyarasam <samkinyara2000@gmail.com>
2022-07-21 12:48:44 +0300

    Socket.io Integration

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+const { isObject } = require('util')
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
     // res.send('<h1>Hello World</h1>')
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
 })
 
 server.listen(8000, () => {
